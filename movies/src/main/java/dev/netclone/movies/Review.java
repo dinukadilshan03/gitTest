ae941c14247b65b12bb76b89e0b35795c3981d0f
dinukadilshan03 <dinukadilshan1105@gmail.com>
2025-03-03 21:38:57 +0530

    v3

--- movies/src/main/java/dev/netclone/movies/Review.java
+++ movies/src/main/java/dev/netclone/movies/Review.java
@@ -7,7 +7,7 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document
+@Document(collection = "reviews")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -19,4 +19,10 @@ public class Review {
     private ObjectId id;
 
     private String body;
+
+    public Review(String body) {
+        this.body = body;
+    }
 }
+
+
